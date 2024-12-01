6d70e6c8fb718a0382b3dcf294df6ccb3e7ffc49
Heralla Karim <kherallakarimo8@gmail.com.com>
2024-12-01 15:27:02 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication34/ConsoleApplication34.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+int* removeNonNegative(int* arr, int& size) {
+    int negativeCount = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            negativeCount++;
+        }
+    }
+    int* newArr = new int[negativeCount];
+    int index = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0) {
+            newArr[index++] = arr[i];
+        }
+    }
+    size = negativeCount;
+    return newArr;
+}
+int main() {
+    int size;
+    cout << "Enter size of the array: ";
+    cin >> size;
+    int* arr = new int[size];
+    cout << "Enter elements of the array:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    int* newArr = removeNonNegative(arr, size);
+    cout << "New array with only negative numbers:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << newArr[i] << " ";
+    }
+    cout << endl;
+    delete[] arr;
+    delete[] newArr;
+    return 0;
+}
